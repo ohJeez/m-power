494ab1555a017d24d41c225292d8d288b1fa8762
ohJeez <jeesthomas48@gmail.com>
2025-07-03 01:02:33 +0530

    nav only

---
+++ script.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const gallery = document.getElementById("gallery");
+  
+//     // You can manually list the image filenames or automate with server-side logic
+//     const imageList = [
+//       "20250701_135443.jpg",
+//       "20250701_101151.jpg",
+//       "20250701_115500.jpg",
+//       "20250701_094255.jpg",
+//       "20250701_122945.jpg",
+//       "20250701_123200.jpg",
+//       "20250701_123043.jpg",
+//       "20250701_101123.jpg",
+//     ];
+  
+//     imageList.forEach((img) => {
+//       const image = document.createElement("img");
+//       image.src = `./images/memories/${img}`;
+//       image.alt = "Memory";
+//       gallery.appendChild(image);
+//     });
+//   });
+// s  
